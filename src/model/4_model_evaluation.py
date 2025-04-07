e9e6f4746972e47bf3b0952afc9ced21f68697dd
sumit kumar <sumitrwk90@gmail.com>
2025-04-07 11:33:12 +0530

    Added Best Model

--- src/model/4_model_evaluation.py
+++ src/model/4_model_evaluation.py
@@ -4,6 +4,8 @@ import numpy as np
 import json
 import pickle
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from dvclive import Live
+import yaml
 
 
 def load_data(filepath : str) -> pd.DataFrame:
@@ -34,12 +36,26 @@ def load_model(filepath : str) -> None:
 
 def evaluation_model(model, X_test : pd.DataFrame, y_test : pd.Series) -> dict:
     try:
+        params = yaml.safe_load(open("params.yaml", "r"))
+        test_size = params["data_collection"]["test_size"]
+        n_estimators = params["model_building"]["n_estimators"]
+
         y_pred = model.predict(X_test)
 
         acc = accuracy_score(y_test, y_pred)
         pre = precision_score(y_test, y_pred)
         recall = recall_score(y_test, y_pred)
         f1score = f1_score(y_test, y_pred)
+
+        with Live(save_dvc_exp = True) as live:
+            live.log_metric("accuracy", acc)
+            live.log_metric("precision", pre)
+            live.log_metric("recall", recall)
+            live.log_metric("f1_score", f1score)
+
+            live.log_param("test_size", test_size)
+            live.log_param("n_estimators", n_estimators)
+
 
         metrics_dict = {
 
