3bd7cf1eb2e675d254819b19fa0ba3408dacb43c
sumit kumar <sumitrwk90@gmail.com>
2025-04-13 18:15:24 +0530

    Added prediction.py, GUI.py, model_registry.py file, and made some changes

--- src/data/2_data_preparation.py
+++ src/data/2_data_preparation.py
@@ -11,12 +11,12 @@ def load_data(filepath : str) -> pd.DataFrame:
 # train_data = pd.read_csv("./data/raw/train.csv")
 # test_data = pd.read_csv("./data/raw/test.csv")
 
-def fill_missing_with_median(df):
+def fill_missing_with_mean(df):
     try:
         for column in df.columns:
             if df[column].isnull().any():
-                median_value = df[column].median()
-                df[column].fillna(median_value, inplace=True)
+                mean_value = df[column].mean()
+                df[column].fillna(mean_value, inplace=True)
 
         return df
     except Exception as e:
@@ -38,8 +38,8 @@ def main():
         train_data = load_data(os.path.join(raw_data_path, "train.csv"))
         test_data = load_data(os.path.join(raw_data_path, "test.csv"))
 
-        train_processed_data = fill_missing_with_median(train_data)
-        test_processed_data = fill_missing_with_median(test_data)
+        train_processed_data = fill_missing_with_mean(train_data)
+        test_processed_data = fill_missing_with_mean(test_data)
 
     # data_path = os.path.join("data", "processed")
 
