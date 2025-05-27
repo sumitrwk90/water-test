85dc809c74fb6ea4a66eae8c41c9b857a80afa20
sumitrwk90 <sumitrwk90@gmail.com>
2025-05-27 15:08:12 +0530

    changed model file path

--- src/model/3_model_building.py
+++ src/model/3_model_building.py
@@ -57,7 +57,7 @@ def main():
     try:
         params_path = "params.yaml"
         data_path = "./data/processed/train_processed.csv"
-        model_name = "models/model.pkl"
+        model_name = "./models/model.pkl"
 
         n_estimators = load_params(params_path)
         train_data = load_data(data_path)
